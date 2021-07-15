17b50b20aed92f3704a71a6ba9bd8e0c9d3911cd
Ben-Tiemeyer <58708695+Ben-Tiemeyer@users.noreply.github.com>
2021-07-15 15:41:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,12 @@ import time
 from urllib.request import urlopen
 import plotly.graph_objs as go
 import datetime
+import gunicorn
+from whitenoise import WhiteNoise
+
+app = dash.Dash(__name__, external_stylesheets = ['/assets/style_sheet.css'])
+server = app.server
+server.wsgi_app = WhiteNoise(server.wsgi_app, root = 'static/')
 
 league_size = 12
 
