123255ace668a413e33a18ce58dea64d40fe66f4
Ben-Tiemeyer <58708695+Ben-Tiemeyer@users.noreply.github.com>
2023-07-21 10:04:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 import matplotlib.pyplot as plt
 from matplotlib import animation
 import dash_table
